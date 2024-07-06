3dd2f1de656409410c2d9723a174eb9d88e05f64
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-07-06 16:47:40 +0200

    design main menu

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -2,26 +2,27 @@ import { Link } from "react-router-dom";
 
 export default function Menu() {
   return (
-    // <div>
-    //   <Link to="/play-quiz">Play Quiz</Link>
-    //   <Link to="/create-quiz">Create Quiz</Link>
-    //   <Link to="/leaderboard">Leaderboard</Link>
-    // </div>
-
-    // <div className="flex h-screen justify-center items-center">
-    <nav className="w-64 bg-gray-800 p-4  flex h-screen w-screen  justify-center items-center">
-      <div className="flex flex-col space-y-4 text-5xl">
-        <Link to="/play-quiz" className="text-white hover:text-gray-300">
+    <nav className="w-64 bg-gray-800 p-4 flex h-screen w-screen justify-center items-center">
+      <div className="flex flex-col space-y-4 text-2xl p-7">
+        <Link
+          to="/play-quiz"
+          className="text-white rounded-lg bg-orange-500 hover:bg-orange-600 text-center p-3 w-80 shadow-lg transform transition-transform duration-200 ease-in-out hover:scale-105"
+        >
           Play Quiz
         </Link>
-        <Link to="/create-quiz" className="text-white hover:text-gray-300">
+        <Link
+          to="/create-quiz"
+          className="text-white rounded-lg bg-orange-500 hover:bg-orange-600 text-center p-3 w-80 shadow-lg transform transition-transform duration-200 ease-in-out hover:scale-105"
+        >
           Create Quiz
         </Link>
-        <Link to="/leaderboard" className="text-white hover:text-gray-300">
+        <Link
+          to="/leaderboard"
+          className="text-white rounded-lg bg-orange-500 hover:bg-orange-600 text-center p-3 w-80 shadow-lg transform transition-transform duration-200 ease-in-out hover:scale-105"
+        >
           Leaderboard
         </Link>
       </div>
     </nav>
-    // </div>
   );
 }
