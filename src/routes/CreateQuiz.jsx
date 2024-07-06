2e478bc2ce974bfad6b61c3e0943688fe10fb969
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-07-06 16:13:50 +0200

    replace for attribute with htmlFor

--- src/routes/CreateQuiz.jsx
+++ src/routes/CreateQuiz.jsx
@@ -125,7 +125,7 @@ export default function CreateQuiz() {
             </select>
 
             <label
-              for="floating_email"
+              htmlFor="floating_email"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-orange-500 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Category
@@ -153,7 +153,7 @@ export default function CreateQuiz() {
             />
 
             <label
-              for="floating_text"
+              htmlFor="floating_text"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-orange-500 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Question
@@ -180,7 +180,7 @@ export default function CreateQuiz() {
               disabled={isLoading}
             />
             <label
-              for="floating_repeat_text"
+              htmlFor="floating_repeat_text"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-orange-500 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Answer A
@@ -208,7 +208,7 @@ export default function CreateQuiz() {
               disabled={isLoading}
             />
             <label
-              for="floating_repeat_text"
+              htmlFor="floating_repeat_text"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-orange-500 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Answer B
@@ -236,7 +236,7 @@ export default function CreateQuiz() {
               disabled={isLoading}
             />
             <label
-              for="floating_repeat_text"
+              htmlFor="floating_repeat_text"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-orange-500 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Answer C
@@ -264,7 +264,7 @@ export default function CreateQuiz() {
               disabled={isLoading}
             />
             <label
-              for="floating_repeat_text"
+              htmlFor="floating_repeat_text"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-orange-500 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Answer D
@@ -278,7 +278,7 @@ export default function CreateQuiz() {
           </div>
 
           <div className="relative z-0 w-full mb-5 group">
-            <label for="underline_select" className="sr-only">
+            <label htmlFor="underline_select" className="sr-only">
               Underline select
             </label>
             <select
@@ -299,7 +299,7 @@ export default function CreateQuiz() {
               <option value="answerD">Answer D</option>
             </select>
             <label
-              for="floating_email"
+              htmlFor="floating_email"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-orange-500 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Correct answer
