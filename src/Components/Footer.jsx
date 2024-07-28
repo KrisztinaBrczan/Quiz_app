1235d1bdb6a4b22fa90c2151d392649b4131b875
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-07-28 14:18:19 +0200

    amend footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import githubSVG from "../assets/github.svg";
 export default function Footer() {
   return (
     <>
-      <div className="fixed bottom-0 right-0 p-4">
+      <div className="fixed bottom-0 right-0 p-4 w-full">
         <a
           href="https://github.com/KrisztinaBrczan/Quiz_app"
           className="  hover:cursor-pointer flex justify-end"
