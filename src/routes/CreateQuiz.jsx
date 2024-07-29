f530b1242d86aa528790c70ebe07e5fce461f878
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-07-29 21:01:43 +0200

    add list questions button back to the quiz amend page

--- src/routes/CreateQuiz.jsx
+++ src/routes/CreateQuiz.jsx
@@ -347,10 +347,7 @@ export default function CreateQuiz({ isUnderUpdating, quizToAmend }) {
           </div>
         </form>
         {/*  */}
-        <div
-          className="flex justify-center "
-          style={{ display: isUnderUpdating ? "none" : "flex" }}
-        >
+        <div className="flex justify-center ">
           <button
             onClick={() => navigate("/all-quiz-questions")}
             className="mb-2 text-orange-500 border-2 border-orange-600 hover:text-orange-600 font-medium rounded-lg text-sm md:text-xl xl:text-2xl 2xl:text-sm  lg:text-base sm:w-auto px-5 py-2.5 text-center dark:focus:border-orange-500 "
