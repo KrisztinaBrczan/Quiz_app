901c2da43a7e9c0aa49a9365053c461040f277b4
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-07-30 19:19:16 +0200

    adjust category chooser to be responsvie

--- src/Components/CategoryChooser.jsx
+++ src/Components/CategoryChooser.jsx
@@ -7,7 +7,7 @@ export default function CategoryChooser({
   return (
     <>
       <div className={`flex justify-center ${showResult ? "hidden" : "flex"}`}>
-        <div className="flex flex-col md:flex-row md:gap-4 gap-2">
+        <div className="flex flex-col sm:flex-row sm:text-xl md:flex-row md:gap-4 lg:text-2xl gap-2">
           <label className="flex items-center space-x-2">
             <input
               className="w-4 h-4 text-orange-500 bg-gray-100 border-gray-300 focus:ring-orange-500 dark:focus:ring-orange-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
