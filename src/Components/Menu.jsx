8c307ce6d3bfe5f443127df38d098a75878a2c71
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-07-30 17:31:46 +0200

    amend main menu

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Menu() {
   return (
     <nav className="w-64 bg-gray-800 p-4 flex h-screen w-screen justify-center items-center">
-      <div className="flex flex-col space-y-4 text-2xl p-7">
+      <div className="flex flex-col space-y-4 text-2xl p-7 xl:text-3xl xl:space-y-5 ">
         <Link
           to="/play-quiz"
           className="text-white rounded-lg bg-orange-500 hover:bg-orange-600 text-center p-3 w-80 shadow-lg transform transition-transform duration-200 ease-in-out hover:scale-105"
